c48dc6d8cb2492f032210755a853d3bacfe113c8
Muhammad Hamza Javed <hamzajaved93333@gmail.com>
2024-08-17 15:35:36 +0500

    Add files via upload

---
+++ Hamza231723 p3.cpp.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int pro;
+int sum;
+int a=10;
+int b=20;
+sum=a+b;
+cout<<"sum="<<sum<<endl;
+a=10;
+b=10;
+pro=a*b;
+cout<<"Product="<<pro;
+return 0;
+}
+
