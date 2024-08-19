004d12b4c878b7a9426a5f97d4948a1ffb45c7bb
Muhammad Hamza Javed <hamzajaved93333@gmail.com>
2024-08-19 18:38:57 +0500

    Add files via upload

---
+++ Hamza231723 p4.cpp.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+using namespace std ;
+int main()       
+	{
+  short testVar = 32767;
+  cout<< testVar<<endl;
+  testVar= testVar+1;
+  cout<< testVar<<endl;
+  testVar= testVar-1;
+  cout<<testVar<<endl;
+  return 0 ;
+  
+    }
