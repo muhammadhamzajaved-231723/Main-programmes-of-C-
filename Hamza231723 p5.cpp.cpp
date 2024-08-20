e3b174eec9d79e56123164dc52b411dff2b0ee5f
Muhammad Hamza Javed <hamzajaved93333@gmail.com>
2024-08-20 17:17:20 +0500

    Add files via upload

---
+++ Hamza231723 p5.cpp.cpp
@@ -0,0 +1,13 @@
+#  include<iostream>
+#  define PI  3.141
+   using namespace std ;
+   int main()    		
+   {
+   float r, area;
+   cout<<"Enter r:";
+   cin>>r;
+   area = 2.0*PI*r;
+   cout<<"Area = "<<area;
+   return 0 ;     	 
+    }
+
