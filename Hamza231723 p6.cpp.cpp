15058afc05fda9fb1fce654d27cbc6bc93f979b5
Muhammad Hamza Javed <hamzajaved93333@gmail.com>
2024-08-21 15:16:06 +0500

    Add files via upload

---
+++ Hamza231723 p6.cpp.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+   int a,b;
+   a=10;
+   b=5;
+  cout<<"a+b="<<a+b<<endl;
+  cout<<"a-b="<<a-b<<endl;
+  cout<<"a*b="<<a*b<<endl;
+  cout<<"a/b="<<a/b<<endl;
+  cout<<"a%b="<<a%b<<endl;
+  return 0;
+}
