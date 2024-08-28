f5a915be68c33ca046e01bd7a1aa7003457d6022
Muhammad Hamza Javed <hamzajaved93333@gmail.com>
2024-08-28 17:14:30 +0500

    Add files via upload

---
+++ Hamza231723 p7.cpp.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    a=10;
+    cout<<"Value of a:"<<a<<endl;
+    a +=5;
+    cout<<"Value of a after a+=5:"<<a<<endl;
+    a-=5;
+    cout<<"Value of a after a-=5:"<<a<<endl;
+    a*=2;
+    cout<<"Value of a after a*=2:"<<a<<endl;
+    a/=2;
+    cout<<"Value of a after a/=2:"<<a<<endl;
+    a%=2;
+    cout<<"Value of a after a%=2:"<<a<<endl;
+
+    return 0;
+ }
