40c5f34afbef15046a9c7b5eaefbbe5b71030b80
Muhammad Hamza Javed <hamzajaved93333@gmail.com>
2024-09-06 16:41:19 +0500

    Add files via upload

---
+++ Hamza231723 p8.cpp.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+ int a,b,x,y;
+ a=b=x=y=0;
+ a++;
+ b=a;
+ ++x;
+ y=x;
+ cout<<"a ="<<a<<endl<<"b ="<<b<<endl;
+ cout<<"x ="<<x<<endl<<"y ="<<y<<endl;
+ 
+  return 0;
+}
---
+++ Hamza231723 p9.cpp.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+   int a,b,x,y;
+   a=b=x=y=0;
+   b=a++;
+   y=++x;
+   cout<<"a ="<<a<<endl<<"b ="<<b<<endl;
+   cout<<"x ="<<x<<endl<<"y ="<<y<<endl;
+    return 0;
+}
